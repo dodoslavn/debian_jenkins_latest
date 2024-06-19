4e826904f10dcc9f692f42580594ba3110346a60
Dodoslav Novak <dodoslavn@gmail.com>
2024-06-19 13:57:49 +0200

    update

---
+++ scripts/update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# load configuration
+source ./config.sh
+
+if [ "$(whoami)" != "$SERVICE_USER" ] && [ "$(whoami)" != "root" ]
+	then
+	echo $(date '+%Y.%m.%d_%H:%M')" - ERROR - You need to be root or service user"
+	exit 1
+	fi
+
+# download latest jenkins
+wget $JENKINS_DL_URL -O $TMP_DL_DIR"jenkins.war"
+
+cmp $TMP_DL_DIR"jenkins.war" $JENKINS_DIR""$JENKINS_WAR
+RC=$?
+
+if [ "$RC" -eq 0 ]
+	then
+	echo $(date '+%Y.%m.%d_%H:%M')" - INFO - There is nothing to update"
+	echo $(date '+%Y.%m.%d_%H:%M')" - INFO - There is nothing to update" >> ../logs/update_$SERVICE_NAME.log
+	exit 0
+	fi
+echo $(date '+%Y.%m.%d_%H:%M')" - INFO - Version updated"
+echo $(date '+%Y.%m.%d_%H:%M')" - INFO - Version updated" >> ../logs/update_$SERVICE_NAME.log
+
+# copy downloaded Jenkins WAR file to your folder
+cp $TMP_DL_DIR"jenkins.war" $JENKINS_DIR""$JENKINS_WAR
+
+systemctl restart $SERVICE_NAME
